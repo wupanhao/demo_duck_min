ec64eae4fda0d6aca537fb3887ee1d7c830a26d3
Panhao Wu <wupanhao@qq.com>
2019-10-15 13:11:38 +0000

    launch file merged

--- catkin_ws/src/05-teleop/dagu_car/src/Car.py
+++ catkin_ws/src/05-teleop/dagu_car/src/Car.py
@@ -4,13 +4,13 @@ import time
 class CarDriver:
 	def __init__(self):
 		self.left = Lepi.MOTOR_5
-		self.right = Lepi.MOTOR_2
+		self.right = Lepi.MOTOR_3
 		Lepi.motor_enable(self.left)
 		Lepi.motor_enable(self.right)
 
 	def setWheelsSpeed(self,left=0,right=0):
-		Lepi.motor_set_speed(self.left,int(-left*25000))
-		Lepi.motor_set_speed(self.right,int(-right*25000))
+		Lepi.motor_set_speed(self.left,int(-left*45000))
+		Lepi.motor_set_speed(self.right,int(-right*45000))
 
 if __name__ == '__main__':
 	car = CarDriver()
